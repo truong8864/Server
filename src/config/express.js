e460fc73180b894c5c78825cddd3204d4487d3f1
truong <nhom1hd1@gmail.com>
2020-09-10 13:45:16 +0700

    ok

--- src/config/express.js
+++ src/config/express.js
@@ -44,8 +44,8 @@ app.use(morgan(logs));
 
 app.use(cookieParser());
 // parse body params and attache them to req.body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // gzip compression
 app.use(compress());
